9a84cbd83f7d669b04859078ed730143f09fddea
priyak0523 <kalaipriya@priya.gateway.2wire.net>
2016-02-25 20:10:56 -0500

    Included test package

--- test/model/AccountTest.java
+++ test/model/AccountTest.java
@@ -38,14 +38,28 @@ public class AccountTest {
 
     @Test
     public void testGetBalanceIs100AfterDeposit500ThenWithdraw400() {
-        
+        double cash = 500.0;
+        Account instance = new Account();
+        instance.deposit(cash);
+        double withdraw=400.0;
+        instance.withdraw(withdraw);
+        double result = instance.getBalance();
+        double expResult = 100.0;
+        assertEquals(expResult, result, 0.0);
     }
     
     @Test
     public void testGetBalanceIsZeroAfterDeposit500ThenClose() 
     {
-        
+        double cash = 500.0;
+        Account instance = new Account();
+        instance.deposit(cash);
+        instance.close(true);
+        double result = instance.getBalance();
+        double expResult = 0.0;
+        assertEquals(expResult, result, 0.0);
     }
+    
     
     
 }
