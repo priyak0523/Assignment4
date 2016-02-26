2d78cca8ee02ccedb6c3a39edd7b3de283cf0ab3
priyak0523 <kalaipriya@priya.gateway.2wire.net>
2016-02-25 19:55:08 -0500

    initial commit

---
+++ src/java/servlet/AccountServlet.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015 Len Payne <len.payne@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Account;
+
+/**
+ * Provides an Account Balance and Basic Withdrawal/Deposit Operations
+ */
+@WebServlet("/account")
+public class AccountServlet extends HttpServlet {
+    Account acc= new Account();
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+response.setHeader("Pragma", "no-cache");
+response.setDateHeader("Expires", 0);
+        try (PrintWriter out = response.getWriter()) {
+       out.println(acc.getBalance());
+    }
+        catch(Exception e){
+            
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        
+         
+        if (request.getParameter("withdraw")!= null)
+        {
+            acc.withdraw(Double.parseDouble(request.getParameter("withdraw")));
+        }
+        
+        if (request.getParameter("deposit")!= null)
+        {
+            acc.deposit(Double.parseDouble(request.getParameter("deposit")));
+        }
+        
+         if (Boolean.parseBoolean(request.getParameter("close")))
+        {
+            acc.close();
+        }
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ test/model/AccountTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author HACKER
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+    
+    @Test
+    public void testGetBalanceIsZeroOnNewAccount() {
+        Account instance = new Account();
+        double expResult = 0.0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void testGetBalanceIs500AfterDeposit500() {
+        double cash = 500.0;
+        Account instance = new Account();
+        instance.deposit(cash);
+        double expResult = 500.0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    @Test
+    public void testGetBalanceIs100AfterDeposit500ThenWithdraw400() {
+        
+    }
+    
+    @Test
+    public void testGetBalanceIsZeroAfterDeposit500ThenClose() 
+    {
+        
+    }
+    
+    
+}
